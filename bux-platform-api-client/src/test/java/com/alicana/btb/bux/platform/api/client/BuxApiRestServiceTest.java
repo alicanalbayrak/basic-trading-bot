b3f21ed9e71bfd71771e75de1fa8142f359aac4d
Ali Can Albayrak <albayrakalican@gmail.com>
2021-03-24 20:05:05 +0100

    Improving code coverage

--- bux-platform-api-client/src/test/java/com/alicana/btb/bux/platform/api/client/BuxApiRestServiceTest.java
+++ bux-platform-api-client/src/test/java/com/alicana/btb/bux/platform/api/client/BuxApiRestServiceTest.java
@@ -1,9 +1,12 @@
 package com.alicana.btb.bux.platform.api.client;
 
 import static com.alicana.btb.bux.platform.api.client.util.AuthorizationTestTokenProvider.getToken;
+import static com.alicana.btb.bux.platform.api.client.util.TestSampleGenerator.restErrBodyString;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import com.alicana.btb.bux.platform.api.client.config.BuxApiConfig;
+import com.alicana.btb.bux.platform.api.client.exception.BuxApiException;
 import com.alicana.btb.bux.platform.api.client.model.BigMoney;
 import com.alicana.btb.bux.platform.api.client.model.OpenPosition;
 import com.alicana.btb.bux.platform.api.client.model.Source;
@@ -12,15 +15,18 @@ import com.alicana.btb.bux.platform.api.client.model.TradeDirection;
 import com.alicana.btb.bux.platform.api.client.util.MockDispatcher;
 import java.io.IOException;
 import java.math.BigDecimal;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class BuxApiRestServiceIT {
+class BuxApiRestServiceTest {
 
-  private static final Logger log = LoggerFactory.getLogger(BuxApiRestServiceIT.class);
+  private static final Logger log = LoggerFactory.getLogger(BuxApiRestServiceTest.class);
 
   private MockWebServer server;
   private BuxApiRestClient buxRestClient;
@@ -56,6 +62,47 @@ class BuxApiRestServiceIT {
 
     Trade closeRequestResult = buxRestClient.closePosition(openReqResult.positionId());
     log.info(closeRequestResult.toString());
+  }
+
+  @Test
+  void shouldSuccessfullyDeserializeErrorBody() {
+
+    // GIVEN
+    this.server.setDispatcher(new Dispatcher() {
+      @Override
+      public MockResponse dispatch(RecordedRequest request) {
+        return new MockResponse().setResponseCode(400).setBody(restErrBodyString());
+      }
+    });
+
+    // WHEN
+    BigMoney bm = new BigMoney("BUX", 2, new BigDecimal(12));
+    Source source = new Source("sourceType", "sourceId");
+    OpenPosition openPos = new OpenPosition("sb26493", bm, 1, TradeDirection.BUY, source);
+
+    // THEN
+    assertThatThrownBy(() -> buxRestClient.openPosition(openPos));
+  }
+
+  @Test
+  void shouldFailToDeserializeErrorBody() {
+
+    // GIVEN
+    this.server.setDispatcher(new Dispatcher() {
+      @Override
+      public MockResponse dispatch(RecordedRequest request) {
+        return new MockResponse().setResponseCode(400).setBody("Poor response");
+      }
+    });
+
+    // WHEN
+    BigMoney bm = new BigMoney("BUX", 2, new BigDecimal(12));
+    Source source = new Source("sourceType", "sourceId");
+    OpenPosition openPos = new OpenPosition("sb26493", bm, 1, TradeDirection.BUY, source);
+
+    // THEN
+    assertThatThrownBy(() -> buxRestClient.openPosition(openPos)).isExactlyInstanceOf(
+        BuxApiException.class);
   }
 
 }
